00dc08527918f8e1c149645231204a011adba3bb
king <refinedking@gmail.com>
2014-03-03 22:23:09 +0800

    Create pyqueue.py
    python队列使用

---
+++ pyqueue.py
@@ -0,0 +1,22 @@
+#coding=utf-8
+from collections import deque
+
+class Q(object):
+	"""docstring for Q
+	Python queue 
+	"""
+
+	queue = []
+	def __init__(self):
+	super(Q, self).__init__()
+
+
+	def q(d):
+		if (isinstance(d,int) || isinstance(d,float)) return;
+		Q.queue.append(d)
+		return Q
+
+	def d():
+		s = Q.queue.popleft();
+		if (!s) return;
+		return s
