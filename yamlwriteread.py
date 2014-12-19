3e15d0b73b16290f6d1c0fc63c445fbc426d7ddf
king <refinedking@gmail.com>
2014-12-19 16:18:31 +0800

    add salt support

--- yamlwriteread.py
+++ yamlwriteread.py
@@ -1,30 +1,25 @@
-# coding=utf-8
 import os
-# easy_install pyyaml
 import yaml
 
-version = open('version.sls')
-versionlist = yaml.load(version)
+def run():
+	# __opts__['pillar_roots']['base'][0]
+	# __opts__['file_roots']['base'][0]
+	a = [ str(i)[0: str(i).rindex('-')] for i in os.listdir(__opts__['pillar_roots']['base'][0] + '/../salt/files')]
+	b = [ str(i)[str(i).rindex('-')+1: str(i).rindex('.')] for i in os.listdir(__opts__['pillar_roots']['base'][0] + '/../salt/files')]
+	c = dict(zip(a,b))
 
-c = [ j for i in os.listdir('../../salt/files') for j in versionlist['version'] if str(i).__contains__(j) ]
-a = list(set(c))
-a.sort(key=c.index)
+	class MagicDict(dict):
+	    def __init__(self, **kwargs):
+	        dict.__init__(self, kwargs)
+	        self.__dict__ = self
+	 
+	    def __getattr__(self, name):
+	        self[name] = MagicDict()
+	        return self[name]
 
-b = [ str(i)[str(i).rindex('-')+1: str(i).rindex('.')] for i in os.listdir('../../salt/files')]
+	d = MagicDict()
+	d.version = c
 
-c = dict(zip(a,b))
-
-class MagicDict(dict):
-    def __init__(self, **kwargs):
-        dict.__init__(self, kwargs)
-        self.__dict__ = self
- 
-    def __getattr__(self, name):
-        self[name] = MagicDict()
-        return self[name]
-
-d = MagicDict()
-d.version = c
-
-stream = file('document.yaml', 'w')
-yaml.dump(dict(d), stream, default_flow_style=False)
+	stream = file(__opts__['pillar_roots']['base'][0] + '/common/version.sls', 'w')
+	yaml.dump(dict(d), stream, default_flow_style=False)
+	return {'buildversion' : 'success'}
